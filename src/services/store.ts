7b5a925c993b1d34cebfe5bbc4dd4176c63024a5
unknown <ritarixxter@yandex.ru>
2023-06-26 17:42:02 +0300

    Реализовала первый запрос и добавила хранилище Redux

---
+++ src/services/store.ts
@@ -0,0 +1,13 @@
+import { configureStore, ThunkAction, Action, ActionCreator, AnyAction } from '@reduxjs/toolkit';
+import { rootReducer } from './slices';
+
+
+const store = configureStore({
+  reducer: rootReducer
+});
+
+export type AppDispatch = typeof store.dispatch;
+export type RootState = ReturnType<typeof store.getState>;
+export type AppThunk<ReturnType = void> = ActionCreator<ThunkAction<ReturnType, RootState, any, AnyAction>>
+
+export default store

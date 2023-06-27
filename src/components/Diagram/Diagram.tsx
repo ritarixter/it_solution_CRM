7b4e49b05961383919ac6fe395ccdb80e886463a
ArsShon2000 <arsenkaribaev98@gmail.com>
2023-06-27 15:45:57 +0300

    Убрал диаграмму из теста

--- src/components/Diagram/Diagram.tsx
+++ src/components/Diagram/Diagram.tsx
@@ -1,36 +1,6 @@
 import { FC } from "react";
-import "chart.js/auto";
-// import { Pie } from "react-chartjs-2";
 import styles from "./Diagram.module.scss";
 
-
-
-// const pieChartData = {
-//   type: "pie",
-//   labels: [" В обработке", "Новые", "Завершенные"],
-//   datasets: [
-//     {
-//       radius: 43.22,
-//       data: [10, 5, 2],
-//       label: "Infected People",
-//       backgroundColor: ["#72DC00", "#F47633", "#E6EAEE"],
-//       hoverBackgroundColor: ["#175000", "#003350", "#993d00"],
-//     },
-//   ],
-//   options: {
-//     // plugins: {
-//       title: {
-//           display: true,
-//           text: "Заявки",
-//           fontSize: 15
-//       },
-//       legend: {
-//         display: true, //Is the legend shown?
-//         position: "top" //Position of the legend.
-//       },
-//     // },
-//   },
-// };
 
 export const Diagram: FC = () => {
 
--- src/pages/Test/Test.tsx
+++ src/pages/Test/Test.tsx
@@ -1,11 +1,8 @@
 import React, {FC, useState} from "react";
-import { Search } from "../../components/Search/Search";
-import { Diagram } from "../../components/Diagram/Diagram";
 
 export const Test: FC = () => {
     return(
         <div>
-            <Diagram />
         </div>
     )
     

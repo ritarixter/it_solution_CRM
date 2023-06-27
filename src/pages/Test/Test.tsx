6fca6c038738cdd214c670d0d6d0f90570bd89ce
ArsShon2000 <arsenkaribaev98@gmail.com>
2023-06-27 17:38:00 +0300

    Изменение в календаре

--- src/pages/Test/Test.tsx
+++ src/pages/Test/Test.tsx
@@ -1,8 +1,10 @@
 import React, {FC, useState} from "react";
+import { CalendarComponent } from "../../components/Calendar/CalendarComponent";
 
 export const Test: FC = () => {
     return(
         <div>
+            <CalendarComponent />
         </div>
     )
     

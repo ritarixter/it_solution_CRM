1c7089dbd2668b2eac032b70a993a67002c9c0b6
ArsShon2000 <arsenkaribaev98@gmail.com>
2023-07-04 17:32:40 +0300

    Все попапы

--- src/pages/Test/Test.tsx
+++ src/pages/Test/Test.tsx
@@ -1,11 +1,16 @@
-import React, {FC, useState} from "react";
-import { CalendarComponent } from "../../components/Calendar/CalendarComponent";
+import { FC } from "react";
+import { EffectMeter } from "../../components/EffectMeter/EffectMeter";
+import {legalDep} from '../../components/EffectMeter/constantsLegalDep';
+import {projectDep} from '../../components/EffectMeter/constantsProjectDep';
+import {purchasingDep} from '../../components/EffectMeter/constantsPurchasingDep';
 
 export const Test: FC = () => {
-    return(
-        <div>
-            <CalendarComponent />
+    return (
+        <div style={{display: "flex"}}>
+            <EffectMeter arr={legalDep} name = {"Юридически отдел"}/>
+            <EffectMeter arr={purchasingDep} name = {"Отдел закупок"}/>
+            <EffectMeter arr={projectDep} name = {"Проектный отдел"}/>
         </div>
     )
-    
+
 }

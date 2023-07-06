ab94b54b2f6a2d9a9b13efc3f4ba3fdfd96e5e60
ArsShon2000 <arsenkaribaev98@gmail.com>
2023-07-06 13:45:52 +0300

    Аналитика

--- src/components/BlockList/BlockList.tsx
+++ src/components/BlockList/BlockList.tsx
@@ -1,29 +1,20 @@
 import { FC } from "react";
 import styles from "./BlockList.module.scss";
+import { EffectMeter } from "../EffectMeter/EffectMeter";
+import { legalDep } from "../EffectMeter/constantsLegalDep";
+import { purchasingDep } from "../EffectMeter/constantsPurchasingDep";
+import { projectDep } from "../EffectMeter/constantsProjectDep";
 
 export const BlockList: FC = () => {
   return (
     <div className={styles.block}>
       <h3 className={styles.block_title}>
-        Количество заявок, которые находяться на различных этапах
+        Эффективность
       </h3>
       <div className={styles.block_stage}>
-        <div>
-          <p className={styles.count}>2</p>
-          <p className={styles.text}>Новые заявки клиентов</p>
-        </div>
-        <div>
-          <p className={styles.count}>12</p>
-          <p className={styles.text}>Заявки в работе у отдела проектирования</p>
-        </div>
-        <div>
-          <p className={styles.count}>8</p>
-          <p className={styles.text}>Заявки в работе у юридического отдела</p>
-        </div>
-        <div>
-          <p className={styles.count}>1</p>
-          <p className={styles.text}>Заявки в работе у юридического отдела</p>
-        </div>
+            <EffectMeter arr={legalDep} name = {"Юридически отдел"}/>
+            <EffectMeter arr={purchasingDep} name = {"Отдел закупок"}/>
+            <EffectMeter arr={projectDep} name = {"Проектный отдел"}/>
       </div>
     </div>
   );

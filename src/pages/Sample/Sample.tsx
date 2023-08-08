edd381a0f42630472f0a3498ee870f247b4392c8
Яковлева Ксения Николаевна <k.yakovleva@itsl.tel>
2023-08-08 12:59:56 +0300

    промежуточный

--- src/pages/Sample/Sample.tsx
+++ src/pages/Sample/Sample.tsx
@@ -1,16 +1,11 @@
-import { FC, useEffect } from "react";
+import { FC} from "react";
 import styles from "./Sample.module.scss";
 import { BlockAddSample } from "../../components/BlockAddSample/BlockAddSample";
 import { HeaderTop } from "../../components/HeaderTop/HeaderTop";
-import { useAppDispatch, useAppSelector } from "../../services/hooks";
-import { getSample } from "../../services/slices/sample";
-import { useNavigate } from "react-router";
+import { useAppSelector } from "../../services/hooks";
 
 export const Sample: FC = () => {
   const {samples} = useAppSelector((state) => state.sample);
-  const dispatch = useAppDispatch();
-  const navigate = useNavigate()
-  const { isAuth, user } = useAppSelector((state) => state.user);
 
   return (
     <div className={styles.block}>

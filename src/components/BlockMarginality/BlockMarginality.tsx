ee78e04867841f2b2d0594df687576bcf9524e59
ArsShon2000 <arsenKaribaev98@gmail.com>
2023-09-12 08:47:46 +0300

    Маржа

--- src/components/BlockMarginality/BlockMarginality.tsx
+++ src/components/BlockMarginality/BlockMarginality.tsx
@@ -3,13 +3,14 @@ import styles from "./BlockMarginality.module.scss";
 
 type TBlockMarginality = {
     style?: boolean
+    marginality?: number
 }
 
-export const BlockMarginality: FC<TBlockMarginality> = ({style}) => {
+export const BlockMarginality: FC<TBlockMarginality> = ({style, marginality}) => {
     return (
         <div className={`${styles.conteiner} ${style && styles.conteiner_style}`}>
             <p className={styles.conteiner_title}>Маржинальность</p>
-            <p className={styles.conteiner_count}>254 354</p>
+            <p className={styles.conteiner_count}>{marginality}</p>
         </div>
     )
 }
--- src/pages/Marginality/Marginality.tsx
+++ src/pages/Marginality/Marginality.tsx
@@ -8,20 +8,37 @@ import { TCommercialProposal } from "../../types";
 import { getByIdCommercialProposalApi } from "../../utils/api";
 import { useLocation, useNavigate } from "react-router";
 import { BlockMarginality } from "../../components/BlockMarginality/BlockMarginality";
+import moment from 'moment';
+import 'moment-weekday-calc';
+moment.locale(); 
+
+declare module "moment" {
+  interface Moment {
+      isoWeekdayCalc(d1: Date | string, d2: Date | string, weekDays: number[], exclusions?: string[], inclusion?: string[]): number
+      weekdayCalc(d1: Date | string, d2: Date | string, weekDays: string[] | number[], exclusions?: string[], inclusion?: string[]): number
+  }
+}
 
 export const Marginality: FC = () => {
   const navigate = useNavigate();
   const location = useLocation();
-  const [startWork, setStartWork] = useState(Date);
-  const [endWork, setEndWork] = useState(Date);
-  const [bribe, setBribe] = useState(String);
-  const [recycling, setRecycling] = useState(String);
-  const [fitter, setFitter] = useState(String);
-  const [ticketHead, setTicketHead] = useState(String);
-  const [ticketFitter, setTicketFitter] = useState(String);
-  const [transport, setTransport] = useState(String);
-  const [housing, setHousing] = useState(String);
-  const [unforeseen, setUnforeseen] = useState(String);
+  const [startWork, setStartWork] = useState<Date>(new Date(moment().format()));
+  const [endWork, setEndWork] = useState<Date>(new Date(moment().format()));
+  const [summaCP, setSummaCP] = useState(398542);                   // сумма кп
+  const [tax, setTax] = useState(summaCP * 0.06);                   // налог
+  const [materialPurchase, setMaterialPurchase] = useState(105000); // Сумма закупки материала
+  const [workingDay, setWorkingDay] = useState(0);                  // Количество рабочих дней
+  const [bribe, setBribe] = useState(0);                            // Взятка
+  const [recycling, setRecycling] = useState(0);                    // Количество переработанных часов
+  const [fitter, setFitter] = useState(2);                          // Количество монтажников
+  const [travelExpenses, setTravelExpenses] = useState(0);          // Затраты на командировочные в день
+  const [fot, setFot] = useState(0);                                // ФОТ монтажников
+  const [ticketHead, setTicketHead] = useState(0);                  // Билеты руководители
+  const [ticketFitter, setTicketFitter] = useState(0);              // Билеты монтажники 
+  const [transport, setTransport] = useState(0);                    // Доставка транспортной
+  const [housing, setHousing] = useState(0);                        // Затраты на жилье
+  const [unforeseen, setUnforeseen] = useState(0);                  // Затраты непредвиденные
+  const [marginality, setMarginality] = useState(0)                 // Маржинальность
   const id_list = Number(location.pathname.slice(21));
   const [CP, setCP] = useState<TCommercialProposal>({
     id: 0,
@@ -36,7 +53,48 @@ export const Marginality: FC = () => {
   //     setCP(res.commercialProposal);
   //   });
   // }, []);
+  const holidays = ['1 Jan ', '2 Jan ', '3 Jan ', '4 Jan ', '5 Jan ', '6 Jan ', '7 Jan ', '8 Jan ', '23 Feb ', '8 Mar ', '1 May ', '9 May ', '12 Jun ', '4 Nov '] // праздники
 
+  useEffect(() => {
+    {
+      //@ts-ignore
+      setWorkingDay(
+        moment().isoWeekdayCalc(
+          startWork,
+          endWork,
+          [1, 2, 3, 4, 5],
+          getExclusionsDates()
+        )
+      );                                                                                                    // расчет календарных дней
+    }
+  }, [startWork, endWork]);
+  
+  useEffect(() => {
+    setTravelExpenses(fitter * 700 * workingDay)
+    setFot(workingDay * 3400 * fitter);
+  }, [workingDay])
+
+  const getExclusionsDates = () => {                                                                        // дни для исключения (праздники)
+    if (new Date(startWork).getFullYear() === new Date(endWork).getFullYear()) 
+      return addYear(new Date(startWork).getFullYear(), holidays)                                           // если год начальной даты и конечной даты в одинаковы
+    else {                                                                                                  // если они разные
+      let multiYear: any= []                                                                                // для календарных дней
+      for (let i = 0; i <= (new Date(endWork).getFullYear() - new Date(startWork).getFullYear()); i++) {    // 2025-2023=2
+        let oldArray: string[] = multiYear                                                                  // копия для конкатенации
+        let yearAdded: string[] = addYear(new Date(startWork).getFullYear() + i, holidays)                  // праздничные дни каждого года
+        multiYear = oldArray.concat(yearAdded)                                                              // конкатенация
+      }
+      return multiYear;                                                                                     // все праздники
+    }
+  }
+
+  const addYear = (year: number, holidays: string[]) => {                                                   //принимает год и массив праздников
+    return holidays.map(item => item + year)                                                                // каждому празднику добавляет год
+  }
+
+  useEffect(() => {
+    setMarginality(+(summaCP - tax - materialPurchase - ticketFitter - ticketHead - transport - housing - travelExpenses - unforeseen - fot).toFixed(2))
+  })
 
   return (
     <>
@@ -49,17 +107,17 @@ export const Marginality: FC = () => {
           <div className={styles.blockSumm}>
             <div className={styles.blockSumm__one}>
               <p className={styles.blockSumm__one_title}>Сумма КП</p>
-              <p className={styles.blockSumm__one_text}>1267</p>
+              <p className={styles.blockSumm__one_text}>{summaCP}</p>
             </div>
             <div className={styles.blockSumm__one}>
               <p className={styles.blockSumm__one_title}>
                 Сумма закупки материала
               </p>
-              <p className={styles.blockSumm__one_text}>108756</p>
+              <p className={styles.blockSumm__one_text}>{materialPurchase}</p>
             </div>
             <div className={styles.blockSumm__one}>
               <p className={styles.blockSumm__one_title}>Налог 6%</p>
-              <p className={styles.blockSumm__one_text}>12670</p>{" "}
+              <p className={styles.blockSumm__one_text}>{tax}</p>{" "}
               {/*Сделать формулу рассчета налога от суммы */}
             </div>
           </div>
@@ -111,7 +169,7 @@ export const Marginality: FC = () => {
                   <Input
                     value={fitter}
                     setValue={setFitter}
-                    type={"text"}
+                    type={"number"}
                     name={"0"}
                     // error={countError}
                   />
@@ -174,11 +232,11 @@ export const Marginality: FC = () => {
               <tr className={styles.tableTwo_row}>
                 <td className={styles.tableTwo_celling}>
                   Количество рабочих дней
-                  <p className={styles.tableTwo_celling_text}>0</p>
+                  <p className={styles.tableTwo_celling_text}>{workingDay}</p>
                 </td>
                 <td className={styles.tableTwo_celling}>
                   ФОТ монтажников
-                  <p className={styles.tableTwo_celling_text}>0</p>
+                  <p className={styles.tableTwo_celling_text}>{fot}</p>
                 </td>
                 <td className={styles.tableTwo_celling}>
                   Количество дней стройки
@@ -188,11 +246,11 @@ export const Marginality: FC = () => {
               <tr className={styles.tableTwo_row}>
                 <td className={styles.tableTwo_celling}>
                   Сумма работ наша окончательная
-                  <p className={styles.tableTwo_celling_text}>0</p>
+                  <p className={styles.tableTwo_celling_text}>{summaCP - tax}</p>
                 </td>
                 <td className={styles.tableTwo_celling}>
                   Затраты на командировочные в день
-                  <p className={styles.tableTwo_celling_text}>0</p>
+                  <p className={styles.tableTwo_celling_text}>{travelExpenses}</p>
                 </td>
                 <td className={styles.tableTwo_celling}>
                   Сумма пеработки
@@ -202,7 +260,7 @@ export const Marginality: FC = () => {
             </table>
           </div>
           <div className={styles.marginality}>
-          <BlockMarginality />
+          <BlockMarginality marginality={marginality} />
           </div>
           <div className={styles.button}>
             <BlockButton

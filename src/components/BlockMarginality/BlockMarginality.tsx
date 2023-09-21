17dd133188403b5f308516909364610d8905dba3
Яковлева Ксения Николаевна <k.yakovleva@itsl.tel>
2023-09-21 13:22:27 +0300

    маржометр

--- src/components/BlockMarginality/BlockMarginality.tsx
+++ src/components/BlockMarginality/BlockMarginality.tsx
@@ -2,13 +2,12 @@ import { FC } from "react";
 import styles from "./BlockMarginality.module.scss";
 
 type TBlockMarginality = {
-    style?: boolean
-    marginality?: number
+    marginality: number
 }
 
-export const BlockMarginality: FC<TBlockMarginality> = ({style, marginality}) => {
+export const BlockMarginality: FC<TBlockMarginality> = ({ marginality}) => {
     return (
-        <div className={`${styles.conteiner} ${style && styles.conteiner_style}`}>
+        <div className={`${styles.conteiner} ${(marginality < 0) && styles.conteiner_style}`}>
             <p className={styles.conteiner_title}>Маржинальность</p>
             <p className={styles.conteiner_count}>{marginality}</p>
         </div>

194510821cef87418ee3637349d17dbea2b9f09e
Margarita Gnezdilova <ritarixxter@yandex.ru>
2023-10-20 17:42:50 +0300

    Update constants.ts

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -15,8 +15,8 @@ export const access: TAccess = {
   PLANNER: "Проектировщик",
 };
 
-//export const accessData = [access.SUPERUSER, access.MANAGER, access.FITTER, access.ENGINEER, access.BUYER, access.VICEPREZIDENT, access.LAWYER, access.PLANNER] //для создания пользователей аксиьни
-export const accessData = [access.FITTER, access.ENGINEER] //для создания пользователей аксиьни
+export const accessData = [access.SUPERUSER, access.MANAGER, access.FITTER, access.ENGINEER, access.BUYER, access.VICEPREZIDENT, access.LAWYER, access.PLANNER] //для создания пользователей аксиьни
+//export const accessData = [access.FITTER, access.ENGINEER] //для создания пользователей аксиьни
 
 export type TAccess = {
   SUPERUSER: string;
